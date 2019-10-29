9617aadf0cebd4bc85860e38ddefc3e85fc3be20
Riley Hale Aeria <rileyhaleaeria@Rileys-MacBook-Pro.local>
2019-10-29 19:08:09 +0800

    Updates

---
+++ precache-manifest.a6cabda2836407dbb2084044b0f07f7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc96cc2df9336b3d81f5e7e0516ab4c6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "0c1bcef0dab6ce3c20b6",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6e5bc5acf16bb478d23d",
+    "url": "/robofriends/static/css/main.c460df95.chunk.css"
+  },
+  {
+    "revision": "0c1bcef0dab6ce3c20b6",
+    "url": "/robofriends/static/js/2.d75c0efc.chunk.js"
+  },
+  {
+    "revision": "6e5bc5acf16bb478d23d",
+    "url": "/robofriends/static/js/main.9fde75c1.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
